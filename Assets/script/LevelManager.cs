dfd5a65bb73cf1bd8581e4230dd829abcf49bd3d
guoxingze <guoxingze@gmail.com>
2015-10-17 19:07:01 -0600

    change blur to 0-5, after 5 move camera to hide

--- Assets/script/LevelManager.cs
+++ Assets/script/LevelManager.cs
@@ -8,10 +8,12 @@ public class LevelManager : MonoBehaviour {
     //public float depth;
 
     public float maxDepth = 100;  // the maximum depth of player
-    public float maxBlur = 20;  // the maximum depth of blur
+    private  float maxBlurDepth = 50;  // the maximum depth of blur
+    private float maxBlurLevel = 5;  // the maximum blur level
+    private int hideCameraZ = 1;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -27,10 +29,20 @@ public class LevelManager : MonoBehaviour {
     /// </summary>
     /// depth: depth of player
     public void setBlur(float depth) {
-
+        float blurLevel = 0;
         //rand of blurSize is 0-10
         //convert depth to 0 - 10
-        float blurLevel = depth /(maxDepth /maxBlur) * 10;  
+        //float blurLevel = depth /(maxDepth /maxBlur)/ maxBlur * 10;  
+        if (depth > maxBlurDepth) {
+            ////blurLevel = 10;
+            environmentCamera.transform.position = new Vector3(environmentCamera.transform.position.x, environmentCamera.transform.position.y, hideCameraZ);
+        } else{
+            blurLevel = depth / maxBlurDepth * maxBlurLevel;
+
+            if (environmentCamera.transform.position.z == hideCameraZ) {
+                environmentCamera.transform.position = new Vector3(environmentCamera.transform.position.x, environmentCamera.transform.position.y, -10);
+            }
+        }
         Debug.Log("blurLevel = " + blurLevel);
         environmentCamera.GetComponent<BlurOptimized>().blurSize = blurLevel;
     }
