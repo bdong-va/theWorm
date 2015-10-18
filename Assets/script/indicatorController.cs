741465df85f88b3645f356b35dccd93328166808
Bo Dong <zillionk1973@gmail.com>
2015-10-17 18:26:42 -0600

    add indicator

---
+++ Assets/script/indicatorController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class indicatorController : MonoBehaviour {
+    public float depth;
+    public float emptyOffset;
+    public float fullOffset;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        foreach (Transform t in transform)
+        {
+            if (t.name == "Core")
+            {
+                t.transform.position = new Vector3(t.transform.position.x, emptyOffset + (depth/100) * (fullOffset - emptyOffset), t.transform.position.z);
+            }
+        }
+    }
+}
