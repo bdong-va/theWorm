8ef8909e45e82f5a409b999c6d8b285bd2c458cd
guoxingze <guoxingze@gmail.com>
2015-10-28 02:05:56 -0600

    setup network

---
+++ Assets/script/PlayerNetworkSetup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+    //unity function when a player joinin the game
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<testPlayer>().enabled = true;
+    }
+}
