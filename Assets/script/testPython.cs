d5f5cd3b0c8587e98cabad1c2de52df5b2449cdf
Rameez <rameez@bigtreeworld.com>
2015-11-12 18:09:12 -0600

    Python with array movement (waaaaay less buggy!)

---
+++ Assets/script/testPython.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testPython : MonoBehaviour {
+
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (v != 0.0f) {
+			v = v / Mathf.Abs(v);
+			Debug.Log (v);
+			GetComponent<Rigidbody2D> ().AddForce (transform.up.normalized * speed * v);
+		}
+		transform.Rotate(0, 0, -h * speed * Time.deltaTime * 90);
+	}
+}
