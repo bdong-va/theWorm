2ad0de7536160de486807350c18fc158e73c6a45
guoxingze <guoxingze@gmail.com>
2015-11-13 21:18:22 -0600

    completed sync player rotation

--- Assets/script/PlayerSyncPosition.cs
+++ Assets/script/PlayerSyncPosition.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 public class PlayerSyncPosition : NetworkBehaviour {
 
     [SyncVar] private Vector3 syncPos; //server will automatically transmit this veriable to all clients when it changes with the SyncVar tag
+    [SyncVar] private Quaternion syncRotation;
 
     [SerializeField] Transform myTransform;
     [SerializeField] float lerpRate = 5;
@@ -17,14 +18,25 @@ public class PlayerSyncPosition : NetworkBehaviour {
 
         //if not local player, and position changes, then get the new position
         LerpPosition();
+        LerpRotation();
 
-	}
+    }
 
     //only set position for the player not local
     void LerpPosition() {
         if (!isLocalPlayer) {
             //set position lerp
             myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    //only set rotation for the player not local
+    void LerpRotation()
+    {
+        if (!isLocalPlayer)
+        {
+            //set position lerp
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, syncRotation, Time.deltaTime * lerpRate);
         }
     }
 
@@ -36,6 +48,15 @@ public class PlayerSyncPosition : NetworkBehaviour {
         syncPos = pos;
     }
 
+    //only run on server
+    //DO NOT remove the 'Cmd' of the function name
+    //tell the server of the players position
+    [Command]
+    void CmdProvideRotationToServer(Quaternion rotation)
+    {
+        syncRotation = rotation;
+    }
+
     //only run on clients
     //tell server the position
     [ClientCallback]
@@ -44,6 +65,7 @@ public class PlayerSyncPosition : NetworkBehaviour {
         if (isLocalPlayer)
         {
             CmdProvidePositionToServer(myTransform.position);
+            CmdProvideRotationToServer(myTransform.rotation);
         }
     }
 }
