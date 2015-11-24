faa0f7fe5d0c0379ed89b9473141fd7f290feffa
Bo Dong <zillionk1973@gmail.com>
2015-11-23 22:47:24 -0600

    add indicator for groundPlayer

---
+++ Assets/script/bin/IndicatorGroundPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndicatorGroundPlayer : MonoBehaviour {
+    public float safeDistance;
+
+    private LevelManager theLevelManager;
+    private Vector3 wormPosition;
+    private float angle;
+    private float distance;
+    private SpriteRenderer arrowRenderer;
+    // private bool wormInDeep;
+    // Use this for initialization
+    void Start () {
+        theLevelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+        wormPosition = theLevelManager.wormPosition;
+        arrowRenderer = transform.Find("WithArrow").GetComponent<SpriteRenderer>();
+        angle = 0;
+        distance = 255;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        if (distance < safeDistance)
+        {
+            float redLevel = (safeDistance - distance) / safeDistance;
+            arrowRenderer.color = new Color(1f, 1 - redLevel, 1 - redLevel, 1f);
+        }
+        else
+        {
+            arrowRenderer.color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+
+    
+    void FixedUpdate() {
+        wormPosition = theLevelManager.wormPosition;
+        angle = 90 + Mathf.Atan2((transform.position.y - wormPosition.y), (transform.position.x - wormPosition.x)) * 180 / Mathf.PI;
+        distance = Vector3.Distance(transform.position, wormPosition);
+    }
+}
